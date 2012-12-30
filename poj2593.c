e7cc2101f3bc6331026f12502451b8130d13938a
Wang Guibao <wang_guibao@163.com>
2012-12-30 17:26:50 +0800

    Solution to POJ2593, similar to POJ2479

---
+++ poj2593.c
@@ -0,0 +1,102 @@
+/*
+ * POJ2593.c - Max Sequence
+ * Problem: 2593    User: LoiteringLeo
+ * emory: 1752K		Time: 204MS
+ * Language: GCC		Result: Accepted
+ */
+#include <stdio.h>
+
+#define MAX_ELE     100000
+#define INFINITY    (100000 * 1000 + 1)
+short ele[MAX_ELE + 1] = {0,};  /* Elements */
+int b[MAX_ELE + 1] = {0,};      /* Max two-subarray sum ending at position i */
+int c[MAX_ELE + 1] = {0,};      /* Max subarray sum ending at position i */
+int d[MAX_ELE + 1] = {0,};      /* max subarray of all sums ending at 1 to i */
+
+/*
+ * lstds - Largest sum of two disjoint subarrays
+ * numbers in array a must begin with a[1] and end with a[n]
+ */
+int lstds(short *a, int n)
+{
+    int i;
+    int max = -INFINITY;
+    b[0] = 0;
+    d[0] = b[0];
+
+    for (i = 1; i <= n; i++) {
+        b[i] = b[i - 1] + a[i];
+        if (b[i] < a[i]) {
+            b[i] = a[i];
+        }
+
+        if (max < b[i]) {
+            max = b[i];
+        }
+        d[i] = max;
+    }
+
+    c[1] = 0;
+    c[2] = a[1] + a[2];
+    max = c[2];
+    for (i = 3; i <= n; i++) {
+        c[i] = c[i - 1] + a[i];
+        if (c[i] < d[i - 1] + a[i]) {
+            c[i] = d[i - 1] + a[i];
+        }
+        if (max < c[i]) {
+            max = c[i];
+        }
+    }
+
+    return max;
+}
+
+int main()
+{
+    int n;
+    int i;
+    int m;
+    int j;
+    int k;
+    int max;
+
+    while (scanf("%d", &n) > 0 && n != 0) {
+        m = 0;
+        j = -INFINITY;
+        k = -INFINITY;
+        for (i = 1; i <= n; i++) {
+            scanf("%hd", &ele[i]);
+            if (ele[i] > 0) {
+                m++;
+            }
+
+            if (j < k) {
+                if (ele[i] > j) {
+                    j = ele[i];
+                }
+            }
+            else {
+                if (ele[i] > k) {
+                    k = ele[i];
+                }
+            }
+
+        }
+
+        if (n == 2) {
+            max = ele[1] + ele[2];
+        }
+        else if (m <= 2) {
+            max = j + k;
+        }
+        else {
+            max = lstds(ele, n);
+        }
+
+        printf("%d\n", max);
+    }
+
+    return 0;
+}
+
